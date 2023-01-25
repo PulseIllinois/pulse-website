41b85379a1b86eb9d59f07c72a9e7ca22e93f937
Gagan Kadadevarmath <gagan2@illinois.edu>
2023-01-25 17:26:59 -0600

    Finished website v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from "./components/Home";
+import Events from "./components/Events";
+import Sponsors from "./components/Sponsors";
+import OurTeam from "./components/OurTeam";
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+          <Navbar />
+
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/sponsors' element={<Sponsors />} />
+            <Route path='/events' element={<Events />} />
+            <Route path="/our-team" element={<OurTeam />} />
+          </Routes>
+
+        
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import { Link, useLocation } from 'react-router-dom';
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  // const [currentPage, setCurrentPage] = useState(window.location.pathname);
+  const loc = useLocation().pathname;
+  return (
+    <nav>
+      <div className={styles.container}>
+        <Link to="/">
+          <img style={{ width: '70px', height: '70px', paddingTop: "20px" }} src={require("../assets/Icon_t.png")} alt="Pulse 2023 Logo" />
+        </Link>
+        <div className={styles.linkContainer}>
+          <h3><Link className={`${styles.link} ${loc === "/" ? styles.active : ''}`} to="/">HOME</Link></h3>
+          <h3><Link className={`${styles.link} ${loc === "/sponsors" ? styles.active : ''}`} to="/sponsors">SPONSORS</Link></h3>
+          <h3><Link className={`${styles.link} ${loc === "/events" ? styles.active : ''}`} to="/events">EVENTS</Link></h3>
+          <h3><Link className={`${styles.link} ${loc === "/our-team" ? styles.active : ''}`} to="/our-team">OUR TEAM</Link></h3>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
